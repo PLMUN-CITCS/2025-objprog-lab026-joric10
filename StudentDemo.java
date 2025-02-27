1997a22407e69dc30c5359772049832b8a153412
joric10 <balconjoric_bscs@plmun.edu.ph>
2025-02-27 17:48:03 +0800

    Submitting OBJPROG Week 05 - Laboratory # 26

---
+++ StudentDemo.java
@@ -0,0 +1,42 @@
+class Student {
+    // Private attributes
+    private String name;
+    private int id;
+    private double gpa;
+    private static int studentCount = 0;
+
+    // Constructor
+    public Student(String name, int id, double gpa) {
+        this.name = name;
+        this.id = id;
+        this.gpa = gpa;
+        studentCount++;
+    }
+
+    // Method to display student information
+    public void displayStudentInfo() {
+        System.out.println("Student ID: " + id + ", Name: " + name + ", GPA: " + gpa);
+    }
+
+    // Static method to display total student count
+    public static void displayStudentCount() {
+        System.out.println("Total students: " + studentCount);
+    }
+}
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        // Creating Student objects
+        Student student1 = new Student("Alice", 101, 3.8);
+        Student student2 = new Student("Bob", 102, 3.5);
+        Student student3 = new Student("Charlie", 103, 3.9);
+
+        // Displaying student information
+        student1.displayStudentInfo();
+        student2.displayStudentInfo();
+        student3.displayStudentInfo();
+
+        // Displaying total student count
+        Student.displayStudentCount();
+    }
+}
